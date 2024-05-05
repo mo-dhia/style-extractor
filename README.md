361b0a46853ac3d9a8fbf1c670e1edad76e4bb84
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2024-05-05 21:06:45 +0100

    alpha

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 
 ## Description
 
-Style Extractor is a versatile VS Code extension designed to enhance your web development workflow. This extension automatically extracts inline CSS styles from your HTML, React, or Svelte components and generates a separate CSS stylesheet. Simply press `Alt+R` and watch as your inline styles are neatly organized into a CSS stylesheet, which is then conveniently copied to your clipboard. Say goodbye to cluttered code files and hello to clean, maintainable code. Boost your productivity and code quality with Style Extractor.
+Style Extractor is a versatile VS Code extension designed to enhance your web development workflow. This extension automatically extracts inline CSS styles from your HTML, React, or Svelte components and generates a separate CSS stylesheet. Simply press `Alt+Q` and watch as your inline styles are neatly organized into a CSS stylesheet, which is then conveniently copied to your clipboard. Say goodbye to cluttered code files and hello to clean, maintainable code. Boost your productivity and code quality with Style Extractor.
+
+!Demo of Style Extractor
 
 ## Usage
 
 1. Open a file with inline styles.
-2. Press `Alt+R`.
+2. Press `Alt+Q`.
 3. The inline styles will be removed from your file and a CSS stylesheet will be copied to your clipboard.
 
+![alt text](https://media.discordapp.net/attachments/1073737355896299542/1236759225032511544/Animation.gif?ex=66392d49&is=6637dbc9&hm=e289a71eb7d2fa39f5ccf7930a1590d7b9f2c70284e8cfe652a8378023e5d320&=&width=1318&height=701)
 ## Contributing
 
 If you have suggestions for how Style Extractor could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
--- extension.js
+++ extension.js
@@ -1,19 +1,128 @@
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
-function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.removeInlineStyles', function () {
+async function activate(context) {
+    let disposable = vscode.commands.registerCommand('extension.removeInlineStyles', async function () {
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return; // No open text editor
         }
 
+        const languageId = editor.document.languageId;
+        console.log('Language ID:', languageId); // Add this line for debugging
+
         let text = editor.document.getText();
-        let css = '';
-        let newText = text.replace(/(class|id)="([^"]+)"\sstyle="([^"]+)"/g, function(_, attr, name, style) {
-            css += attr === 'class' ? '.' : '#';
-            css += `${name} { ${style} }\n`;
-            return `${attr}="${name}"`;
-        });
+        let newText = ''
+        let css = ''
+        const styles = []
+
+
+        function converter(input, type, isReact) {
+            // Remove inline styles
+            const stylePattern = isReact ? /(\s*style={{[^}]+}})/g : /(style="[^"]*")|(style='[^']*')/g
+
+            input = input.replace(stylePattern, (stylesString, i) => {
+                const complex = [];
+                let stylesArray = stylesString.split(isReact ? ',' : ';');
+                let stylesAcc = ''
+                for (let style of stylesArray) {
+                    // Split the style into property and value
+
+                    if (!isReact) {
+                        style = style.replace(/style=("[^"]*"|'[^']*')?/g, '').replace(/\n/g, '').replace(/ /g, '').replace(/"/g, '').replace(/'/g, '');
+                    }
+                    if (!style) continue
+
+                    const splitValue = style.split(':')
+                    let [property, value] = splitValue;
+
+                    if (isReact) {
+                        property = property.replace(/\s/g, '').replace(/\n/g, '').replace(/}/g, '').replace(/style={{/g, '')
+                    }
+                    // Skip if property or value is undefined
+                    if (!property || !value) continue;
+
+                    let clearedValue
+                    if (isReact) {
+                        clearedValue = splitValue.slice(1).join(':').replace(/\s/g, '').replace(/\n/g, '').replace(/}/g, '').replace(/style={{/g, '')
+
+                        if ((clearedValue[0] === '"' && clearedValue[clearedValue.length - 1] === '"') ||
+                            (clearedValue[0] === "'" && clearedValue[clearedValue.length - 1] === "'")) {
+                            let cssProperty = property.replace(/([A-Z])/g, "-$1").toLowerCase();
+                            stylesAcc += ` ${cssProperty}: ${clearedValue.slice(1, -1)};`;
+                        } else if (isNaN(Number(clearedValue))) {
+                            complex.push(`${property}: ${clearedValue}`);
+                        } else {
+                            let cssProperty = property.replace(/([A-Z])/g, "-$1").toLowerCase();
+                            stylesAcc += ` ${cssProperty}: ${clearedValue};`;
+                        }
+
+                    } else {
+                        if (value.includes('{') || value.includes('}') || value.includes('?') || value.includes('$')) {
+                            complex.push(property + ':' + splitValue.slice(1).join(':'))
+                        } else {
+                            stylesAcc += (' ' + property + ': ' + value + ';')
+                        }
+                    }
+                }
+
+
+                styles.push(stylesAcc)
+                if (complex.length) {
+                    if (isReact) {
+                        return ` style={{${complex.join(', ')}}}`;
+                    } else {
+                        return ` style='${complex.join('; ')}'`;
+                    }
+                } else {
+                    return '';
+                }
+            });
+
+
+
+            // Remove class names and ids with invalid characters
+            const classIdPattern = isReact ? / (?:className|id)=['"]([^'"]+)['"]/g : / (?:class|id)=['"]([^'"]+)['"]/g
+            let index = 0
+            input = input.replace(classIdPattern, (match, attribute) => {
+                const className = match.includes('id=') ? '#' + attribute : '.' + attribute
+                css += className + ' {' + styles[index] + ' }' + '\n'
+                index++
+                if (!attribute.includes(' ') && !attribute.includes('>') && !attribute.includes(':')) {
+
+                    if (type === 'next' || type === 'react-native') {
+                        return ` className={styles.${attribute}}`
+                    } else {
+                        return match
+                    }
+
+                } else {
+                    return ''
+                }
+            });
+            return input;
+        }
+
+        const findStyles = (type, isReact) => {
+            newText = converter(text, type, isReact)
+        };
+
+        // Await getting the project type
+        const type = await getProjectType();
+
+        if (languageId === 'javascript' || languageId === 'typescript') {
+            // React JSX or JS file
+            findStyles(type, true)
+        } else if (languageId === 'html') {
+            findStyles('html', true)
+
+        } else if (languageId === 'svelte') {
+            findStyles('svelte', true)
+        } else {
+            vscode.window.showErrorMessage('Css extractor could not find type of the project');
+            return;
+        }
 
         editor.edit(editBuilder => {
             const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
@@ -28,9 +137,64 @@ function activate(context) {
     context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
+function deactivate() { }
+
+async function getProjectType() {
+    // Get the path of the current file
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor || !activeEditor.document) {
+        // No active editor
+        return false;
+    }
+
+    const currentFilePath = activeEditor.document.fileName;
+
+    try {
+        // Navigate up from the current file to find package.json
+        let currentDir = path.dirname(currentFilePath);
+        let packageJsonPath;
+
+        // Loop until we find package.json or reach the root directory
+        while (true) {
+            packageJsonPath = path.join(currentDir, 'package.json');
+            if (fs.existsSync(packageJsonPath)) {
+                break;
+            }
+
+            // Go up one directory
+            const parentDir = path.dirname(currentDir);
+            if (parentDir === currentDir) {
+                // Reached root directory
+                return false;
+            }
+            currentDir = parentDir;
+        }
+
+        const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf-8');
+        const packageJson = JSON.parse(packageJsonContent);
+
+        // Check if it has Next.js dependencies
+        if (packageJson['dependencies'] && packageJson['dependencies']['react']) {
+            if (packageJson['dependencies']['next'] || packageJson['dependencies']['next/dist/server']) {
+                return 'next'
+            } else if (packageJson['dependencies']['react-native']) {
+                return 'react-native'
+            } else {
+                return 'react'
+            }
+        }
+
+        // If you need to check for other Next.js dependencies, add them here
+
+        return false;
+    } catch (error) {
+        // Error reading package.json or parsing JSON
+        console.error(error);
+        return false;
+    }
+}
 
 module.exports = {
     activate,
     deactivate
-}
+};
